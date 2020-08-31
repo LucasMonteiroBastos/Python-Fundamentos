7d3047ba0743c0b65574134bcca68822b9a32cbb
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-08-31 11:31:56 -0300

    Operações matematicas com Python 01

---
+++ Números e Operações Matemáticas com Jupyter Notebook 01.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## numeros e operações matematicas 
+
+# In[1]:
+
+
+# soma
+4 + 4
+
+
+# In[2]:
+
+
+# subtração 
+4 - 2
+
+
+# In[3]:
+
+
+# multiplicação 
+4 * 4
+
+
+# In[4]:
+
+
+# divisão 
+4 / 2
+
+
+# In[5]:
+
+
+# potencia 
+4 ** 4
+
+
+# In[7]:
+
+
+# mode 
+10 % 3
+
+
+# ## função type
+
+# In[8]:
+
+
+type(5)
+
+
+# In[9]:
+
+
+type(5.0)
+
+
+# In[11]:
+
+
+a = 'Eu sou uma string'
+type(a)
+
+
+# ## Operações com numeros float / double
+
+# In[12]:
+
+
+3.1 + 4.5
+
+
+# In[13]:
+
+
+4 + 8.0
+
+
+# ## conversão
+
+# In[14]:
+
+
+float(9)
+
+
+# In[15]:
+
+
+int(6.0)
+
+
+# In[16]:
+
+
+int(6.5)
+
+
+# ## hexadicimal e binario
+
+# In[17]:
+
+
+hex(394)
+
+
+# In[18]:
+
+
+hex(217)
+
+
+# In[19]:
+
+
+bin(286)
+
+
+# In[20]:
+
+
+bin(390)
+
+
+# In[ ]:
+
+
+
+
